1d2999800738cde0a3ab252e84d73a6e6d120386
Matthew X. Economou <xenophon@irtnog.org>
2020-10-16 09:54:01 -0400

    feat: limit alerts to one every 24 hours
    Prevent the service from spamming notifications by keeping a record of
    when a station with a high price was last seen.  Tracking the BGS tick
    seems like too much effort, plus it would add a dependency on the
    EliteBGS service.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const zlib = require('zlib');
 const zmq = require('zeromq/v5-compat');
 const sock = zmq.socket('sub');
+var seen = new Map();
 
 sock.connect('tcp://eddn.edcd.io:9500');
 sock.subscribe('');
@@ -11,12 +12,15 @@ sock.on('message', topic => {
     if (datum['$schemaRef'] == 'https://eddn.edcd.io/schemas/commodity/3') {
         const system = datum.message.systemName;
         const station = datum.message.stationName;
+        const timestamp = Math.floor(Date.now() / 1000);
         for (const commodity of datum.message.commodities) {
             if (commodity.name == 'painite') {
                 const price = commodity.sellPrice;
                 const demand = commodity.demand;
-                if (price >= 950000 && demand >= 1000) {
+                if (price >= 950000 && demand >= 1000 && !seen[system+station] || seen[system+station] < timestamp) {
                     console.log(station + ' in ' + system + ' buying ' + demand + ' tonnes painite for ' + price + ' CR');
+                    // limit alerts to one every 24 hours
+                    seen[system+station] = timestamp + 86400;
                 }
                 break;
             }
