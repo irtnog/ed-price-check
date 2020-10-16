3081fbdceaa61a413b783ee2f13f9de4ecf12a17
Matthew X. Economou <xenophon@irtnog.org>
2020-10-16 13:52:55 -0400

    feat: send alerts to an Amazon SNS topic

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const AWS = require('aws-sdk');
 const zlib = require('zlib');
 const zmq = require('zeromq/v5-compat');
 const sock = zmq.socket('sub');
+
+var sns = new AWS.SNS({region: process.env.AWS_DEFAULT_REGION});
 var seen = new Map();
 
 sock.connect('tcp://eddn.edcd.io:9500');
@@ -18,7 +21,12 @@ sock.on('message', topic => {
                 const price = commodity.sellPrice;
                 const demand = commodity.demand;
                 if (price >= 950000 && demand >= 1000 && !seen[system+station] || seen[system+station] < timestamp) {
-                    console.log(station + ' in ' + system + ' buying ' + demand + ' tonnes painite for ' + price + ' CR');
+                    let message = station + ' in ' + system + ' buying ' + demand + ' tonnes painite for ' + price + ' CR';
+                    console.log(message);
+                    sns.publish({TopicArn: process.env.TOPIC_ARN, Message: message}, (err, data) => {
+                        if (err) console.log(err, err.stack);
+                        else     console.log(data);
+                    });
                     // limit alerts to one every 24 hours
                     seen[system+station] = timestamp + 86400;
                 }
